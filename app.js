737b5fc99ad24c543170a51494ec317e17db4897
unknown <hbpsnape@yahoo.com>
2022-06-07 00:28:41 +0600

    showing background color as hex number in the text field

--- app.js
+++ app.js
@@ -14,10 +14,12 @@ window.onload = () => {
 function main(){
     const root = document.getElementById('root')
     const btn = document.getElementById('change-btn')
+    const output = document.getElementById('output')
 
     btn.addEventListener('click', function(){
         const bgColor = generateRGBColor()
         root.style.backgroundColor = bgColor;
+        output.value = bgColor;
     })
 }
 
@@ -28,7 +30,7 @@ function generateRGBColor(){
     const green = Math.floor(Math.random()*255);
     const blue = Math.floor(Math.random()*255);
 
-    return `rgb(${red}, ${green}, ${blue})`
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
 }
 // Step 3 - Collect all necessary references
 
